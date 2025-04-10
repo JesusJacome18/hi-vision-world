f0f6f7f7977df913e3dc839d7ee03b83bd5a27c0
JesusJacome18 <jejacome@unal.edu.co>
2025-04-10 15:41:41 -0500

    ab

---
+++ Gray-to-RGB.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+I= np.array([[[0. 255, 0]], [[0, 128, 128]], [[128, 0, 0]], [[128, 0, 128]]])
+
+print(I.shape)
+
+#plt.imshow(I, vmin=0, vmax=255)
+#plt.show()
